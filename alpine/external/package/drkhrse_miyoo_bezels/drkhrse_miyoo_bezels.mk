685bce33bb1bc99f86c98ad002fd3ccd3b79cfa3
jheronimus <jheronimus@users.noreply.github.com>
2026-05-30 03:47:25 +0300

    fix: correct git branch name and flatten bezel installation path for drkhrse_miyoo_bezels

--- alpine/external/package/drkhrse_miyoo_bezels/drkhrse_miyoo_bezels.mk
+++ alpine/external/package/drkhrse_miyoo_bezels/drkhrse_miyoo_bezels.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DRKHRSE_MIYOO_BEZELS_VERSION = master
+DRKHRSE_MIYOO_BEZELS_VERSION = main
 DRKHRSE_MIYOO_BEZELS_SITE = https://github.com/drkhrse/drkhrse_miyoo_bezels.git
 DRKHRSE_MIYOO_BEZELS_SITE_METHOD = git
 DRKHRSE_MIYOO_BEZELS_LICENSE = GPL-3.0
@@ -15,7 +15,7 @@ DRKHRSE_MIYOO_BEZELS_INSTALL_IMAGES = YES
 define DRKHRSE_MIYOO_BEZELS_INSTALL_IMAGES_CMDS
 	mkdir -p $(BINARIES_DIR)/ui/bezels
 	# Copy bezel assets to the staging ui directory
-	cp -rf $(@D)/* $(BINARIES_DIR)/ui/bezels/
+	cp -rf $(@D)/drkhrse_miyoo_bezels/* $(BINARIES_DIR)/ui/bezels/
 endef
 
 $(eval $(generic-package))
