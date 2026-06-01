b528201543d6a26bf66aff4ce0593bd099fef65a
jheronimus <jheronimus@users.noreply.github.com>
2026-06-01 10:57:52 +0300

    buildroot: configure dynamic libmali selection, fix GCC 14 build error

--- alpine/external/external.mk
+++ alpine/external/external.mk
@@ -17,3 +17,24 @@ LINUX_POST_CONFIGURE_HOOKS += MINIME_PATCH_LINUX_CONFIG
 
 SDL2_AUTORECONF = YES
 
+ifeq ($(BR2_PACKAGE_LIBMALI),y)
+SDL2_CONF_OPTS += --enable-video-mali
+else
+SDL2_CONF_OPTS += --disable-video-mali
+endif
+
+define SDL2_RESTORE_CONFIG_H
+	mv $(@D)/include/SDL_config.h $(@D)/include/SDL_config.h.orig
+	(echo '#ifndef SDL_config_h_'; \
+	 echo '#define SDL_config_h_'; \
+	 echo '#include "SDL_platform.h"'; \
+	 cat $(@D)/include/SDL_config.h.orig; \
+	 echo '#endif /* SDL_config_h_ */') > $(@D)/include/SDL_config.h
+endef
+SDL2_POST_CONFIGURE_HOOKS += SDL2_RESTORE_CONFIG_H
+
+define SDL2_ADD_MALI_SOURCES
+	$(SED) '/AC_DEFINE(SDL_VIDEO_DRIVER_MALI/a \            SOURCES="$$SOURCES $$srcdir/src/video/mali-fbdev/*.c"' $(@D)/configure.ac
+	python3 $(BR2_EXTERNAL_MINIME_PATH)/../scripts/patch_mali_dma_heap.py $(@D)
+endef
+SDL2_POST_PATCH_HOOKS += SDL2_ADD_MALI_SOURCES
