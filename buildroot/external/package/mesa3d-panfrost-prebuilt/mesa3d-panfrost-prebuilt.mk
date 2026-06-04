d7f72e5690685c6635e19cd603d46178fd752f49
jheronimus <jheronimus@users.noreply.github.com>
2026-06-05 02:50:04 +0300

    feat(gpu): Add Mesa/LLVM prebuilt integration for dynamic GPU driver switching

---
+++ buildroot/external/package/mesa3d-panfrost-prebuilt/mesa3d-panfrost-prebuilt.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# mesa3d-panfrost-prebuilt
+#
+################################################################################
+
+MESA3D_PANFROST_PREBUILT_VERSION = 25.0.7
+MESA3D_PANFROST_PREBUILT_SITE = $(call github,jheronimus,panfrost-prebuilts,v$(MESA3D_PANFROST_PREBUILT_VERSION))
+MESA3D_PANFROST_PREBUILT_LICENSE = MIT, GPL-2.0, LGPL-2.1
+MESA3D_PANFROST_PREBUILT_LICENSE_FILES = COPYING
+
+define MESA3D_PANFROST_PREBUILT_INSTALL_TARGET_CMDS
+	# Extract precompiled libraries directly into /usr/lib/panfrost on the target
+	mkdir -p $(TARGET_DIR)/usr/lib/panfrost
+	tar -C $(TARGET_DIR)/usr/lib/panfrost --strip-components=1 \
+		-xzf $(MESA3D_PANFROST_PREBUILT_DIR)/$(MESA3D_PANFROST_PREBUILT_SOURCE)
+endef
+
+$(eval $(generic-package))
