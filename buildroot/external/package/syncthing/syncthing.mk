f5ee0e5899b25a6a9073acec14c6a2cdfff0f2e3
jheronimus <jheronimus@users.noreply.github.com>
2026-06-15 22:03:24 +0300

    syncthing: remove broken calmh/go-sqlite3 fork replace directive

--- buildroot/external/package/syncthing/syncthing.mk
+++ buildroot/external/package/syncthing/syncthing.mk
@@ -11,4 +11,9 @@ SYNCTHING_LICENSE_FILES = LICENSE
 SYNCTHING_GOMOD = github.com/syncthing/syncthing
 SYNCTHING_BUILD_TARGETS = cmd/syncthing
 
+define SYNCTHING_REMOVE_BROKEN_SQLITE_REPLACE
+	sed -i '/calmh\/go-sqlite3/d' $(@D)/go.mod
+endef
+SYNCTHING_POST_PATCH_HOOKS += SYNCTHING_REMOVE_BROKEN_SQLITE_REPLACE
+
 $(eval $(golang-package))
