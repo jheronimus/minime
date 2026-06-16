ff9e1c467fc1bb4c3cf0925455a1e522f1007ef4
jheronimus <jheronimus@users.noreply.github.com>
2026-06-17 02:45:29 +0300

    Configure fsck, preloaded ROMs & BIOS staging, passwordless FTP, set rk3566 default BOARD

--- buildroot/external/package/preloaded-roms/preloaded-roms.mk
+++ buildroot/external/package/preloaded-roms/preloaded-roms.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PRELOADED_ROMS_VERSION = 413e0c74465a03ece75a86d7957ff4a84afcab61
+PRELOADED_ROMS_VERSION = cc85d1accc3b3fb2d65786edc62d6604ea19f0b9
 PRELOADED_ROMS_SITE = https://github.com/minime-os/roms.git
 PRELOADED_ROMS_SITE_METHOD = git
 PRELOADED_ROMS_LICENSE = Free / Shareware
@@ -15,90 +15,43 @@ PRELOADED_ROMS_TARGET_ROMS_DIR = $(BINARIES_DIR)/ui/roms
 define PRELOADED_ROMS_INSTALL_TARGET_CMDS
 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)"
 	
-	# Copy console ROM folders if they are selected in Buildroot
-	if [ "$(BR2_PACKAGE_LIBRETRO_FCEUMM)" = "y" ] && [ -d "$(@D)/Roms/Nintendo Entertainment System (FC)" ]; then \
+	# Copy console ROM folders if they exist in the repository
+	if [ -d "$(@D)/nes" ]; then \
 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/nes"; \
-		cp -a "$(@D)/Roms/Nintendo Entertainment System (FC)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/nes/"; \
+		cp -a "$(@D)/nes/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/nes/"; \
 	fi
-	if [ "$(BR2_PACKAGE_LIBRETRO_GAMBATTE)" = "y" ]; then \
-		if [ -d "$(@D)/Roms/Game Boy (GB)" ]; then \
-			mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gb"; \
-			cp -a "$(@D)/Roms/Game Boy (GB)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gb/"; \
-		fi; \
-		if [ -d "$(@D)/Roms/Game Boy Color (GBC)" ]; then \
-			mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gb"; \
-			cp -a "$(@D)/Roms/Game Boy Color (GBC)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gb/"; \
-		fi; \
+	if [ -d "$(@D)/gb" ]; then \
+		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gb"; \
+		cp -a "$(@D)/gb/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gb/"; \
 	fi
-	if { [ "$(BR2_PACKAGE_LIBRETRO_GPSP)" = "y" ] || [ "$(BR2_PACKAGE_LIBRETRO_MGBA)" = "y" ]; } && [ -d "$(@D)/Roms/Game Boy Advance (GBA)" ]; then \
+	if [ -d "$(@D)/gba" ]; then \
 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gba"; \
-		cp -a "$(@D)/Roms/Game Boy Advance (GBA)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gba/"; \
+		cp -a "$(@D)/gba/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gba/"; \
 	fi
-	if [ "$(BR2_PACKAGE_LIBRETRO_PICODRIVE)" = "y" ]; then \
-		if [ -d "$(@D)/Roms/Sega Genesis (MD)" ]; then \
-			mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/md"; \
-			cp -a "$(@D)/Roms/Sega Genesis (MD)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/md/"; \
-		fi; \
-		if [ -d "$(@D)/Roms/Sega Game Gear (GG)" ]; then \
-			mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gg"; \
-			cp -a "$(@D)/Roms/Sega Game Gear (GG)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gg/"; \
-		fi; \
-		if [ -d "$(@D)/Roms/Sega Master System (SMS)" ]; then \
-			mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/sms"; \
-			cp -a "$(@D)/Roms/Sega Master System (SMS)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/sms/"; \
-		fi; \
+	if [ -d "$(@D)/md" ]; then \
+		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/md"; \
+		cp -a "$(@D)/md/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/md/"; \
 	fi
-	if [ "$(BR2_PACKAGE_LIBRETRO_PCSX_REARMED)" = "y" ] && [ -d "$(@D)/Roms/Sony PlayStation (PS)" ]; then \
+	if [ -d "$(@D)/gg" ]; then \
+		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gg"; \
+		cp -a "$(@D)/gg/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/gg/"; \
+	fi
+	if [ -d "$(@D)/sms" ]; then \
+		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/sms"; \
+		cp -a "$(@D)/sms/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/sms/"; \
+	fi
+	if [ -d "$(@D)/psx" ]; then \
 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/psx"; \
-		cp -a "$(@D)/Roms/Sony PlayStation (PS)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/psx/"; \
+		cp -a "$(@D)/psx/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/psx/"; \
 	fi
-	if [ "$(BR2_PACKAGE_LIBRETRO_MEDNAFEN_PCE_FAST)" = "y" ] && [ -d "$(@D)/Roms/TurboGrafx-16 (PCE)" ]; then \
+	if [ -d "$(@D)/pce" ]; then \
 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pce"; \
-		cp -a "$(@D)/Roms/TurboGrafx-16 (PCE)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pce/"; \
+		cp -a "$(@D)/pce/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pce/"; \
 	fi
-	if { [ "$(BR2_PACKAGE_LIBRETRO_MEDNAFEN_SUPAFAUST)" = "y" ] || [ "$(BR2_PACKAGE_LIBRETRO_SNES9X2005_PLUS)" = "y" ]; } && [ -d "$(@D)/Roms/Super Nintendo Entertainment System (SFC)" ]; then \
+	if [ -d "$(@D)/snes" ]; then \
 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/snes"; \
-		cp -a "$(@D)/Roms/Super Nintendo Entertainment System (SFC)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/snes/"; \
+		cp -a "$(@D)/snes/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/snes/"; \
 	fi
-	# Commented out systems (no emulators shipped yet):
-	# if [ "$(BR2_PACKAGE_LIBRETRO_RACE)" = "y" ]; then \
-	# 	if [ -d "$(@D)/Roms/Neo Geo Pocket (NGP)" ]; then \
-	# 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/ngp"; \
-	# 		cp -a "$(@D)/Roms/Neo Geo Pocket (NGP)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/ngp/"; \
-	# 	fi; \
-	# 	if [ -d "$(@D)/Roms/Neo Geo Pocket Color (NGPC)" ]; then \
-	# 		mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/ngp"; \
-	# 		cp -a "$(@D)/Roms/Neo Geo Pocket Color (NGPC)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/ngp/"; \
-	# 	fi; \
-	# fi
-	# if [ "$(BR2_PACKAGE_LIBRETRO_MEDNAFEN_VB)" = "y" ] && [ -d "$(@D)/Roms/Virtual Boy (VB)" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/vb"; \
-	# 	cp -a "$(@D)/Roms/Virtual Boy (VB)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/vb/"; \
-	# 	fi
-	# if [ "$(BR2_PACKAGE_LIBRETRO_POKEMINI)" = "y" ] && [ -d "$(@D)/Roms/Pokemon mini (PKM)" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pkm"; \
-	# 	cp -a "$(@D)/Roms/Pokemon mini (PKM)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pkm/"; \
-	# fi
-	# if [ "$(BR2_PACKAGE_LIBRETRO_FAKE08)" = "y" ] && [ -d "$(@D)/Roms/Pico-8 (P8)" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pico8"; \
-	# 	cp -a "$(@D)/Roms/Pico-8 (P8)/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/pico8/"; \
-	# fi
-	# if [ -d "$(@D)/Roms/Atari Lynx" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/lynx"; \
-	# 	cp -a "$(@D)/Roms/Atari Lynx/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/lynx/"; \
-	# fi
-	# if [ -d "$(@D)/Roms/Wonderswan" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/wswan"; \
-	# 	cp -a "$(@D)/Roms/Wonderswan/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/wswan/"; \
-	# fi
-	# if [ -d "$(@D)/Roms/Mega Duck" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/mduck"; \
-	# 	cp -a "$(@D)/Roms/Mega Duck/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/mduck/"; \
-	# fi
-	# if [ -d "$(@D)/Roms/Watara Supervision" ]; then \
-	# 	mkdir -p "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/watara"; \
-	# 	cp -a "$(@D)/Roms/Watara Supervision/." "$(PRELOADED_ROMS_TARGET_ROMS_DIR)/watara/"; \
-	# fi
 endef
 
 $(eval $(generic-package))
