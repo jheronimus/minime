6700047ae9595fe3adbcc67fc7fdaf61e4288e65
jheronimus <jheronimus@users.noreply.github.com>
2026-07-19 18:19:08 +0300

    Switch minui to fetch-prebuilt zip; drop retroarch/libretro-cores/drkhrse-bezels
    Phase 2 (traits):
    - Add quirks key as recognized optional trait (deferred, no ini changes)
    
    Phase 3 (packages):
    - buildroot/minui.mk: rewrite as fetch-prebuilt from GitHub releases
    - alpine/minui/APKBUILD: rewrite as fetch-prebuilt, stage to /alpine-output/boot/ui/
    - minui Config.in: remove libretro-headers select (not a runtime dep)
    - Board configs: remove libretro-cores, drkhrse-miyoo-bezels
    - Alpine build.sh, world-common: remove retroarch, libretro-cores, drkhrse-miyoo-bezels

--- buildroot/external/package/minui/minui.mk
+++ buildroot/external/package/minui/minui.mk
@@ -4,199 +4,39 @@
 #
 ################################################################################
 
-MINUI_VERSION = ac7ee447b6f417239923f5a1511790dec6d21d1c
-MINUI_SITE = https://github.com/jheronimus/MinUI.git
-MINUI_SITE_METHOD = git
+MINUI_VERSION = v20260719
+MINUI_DATE = $(MINUI_VERSION:v%=%)
+MINUI_DOT_NUMBER = 0
+MINUI_RELEASE_BASE = MinUI-$(MINUI_DATE)-$(MINUI_DOT_NUMBER)
+MINUI_SITE = https://github.com/jheronimus/MinUI/releases/download/$(MINUI_VERSION)
+MINUI_SOURCE = $(MINUI_RELEASE_BASE)-base.zip
+MINUI_SITE_METHOD = wget
 MINUI_LICENSE = See upstream
-MINUI_LICENSE_FILES = README.md
+MINUI_LICENSE_FILES = README.txt
 
-MINUI_DEPENDENCIES = dbus libretro-headers lz4 sdl2 sdl2_image sdl2_ttf zlib
+MINUI_DEPENDENCIES =
 MINUI_INSTALL_IMAGES = YES
-
-# Minime SD-card layout contract. These values are baked into the UI binaries
-# at build time so that the UI package does not need to know the firmware
-# internals at runtime. S60ui still exports the same values for subprocesses.
-MINUI_SDCARD_PATH = /mnt/sdcard
-MINUI_UI_PATH = $(MINUI_SDCARD_PATH)/.ui
-MINUI_CORES_PATH = $(MINUI_SDCARD_PATH)/.cores
-
-MINUI_CPPFLAGS = \
-	-DSDCARD_PATH=\"$(MINUI_SDCARD_PATH)\" \
-	-DSYSTEM_PATH=\"$(MINUI_UI_PATH)\" \
-	-DROOT_SYSTEM_PATH=\"$(MINUI_UI_PATH)\" \
-	-DRES_PATH=\"$(MINUI_UI_PATH)/res\" \
-	-DCORE_CONFIGS_PATH=\"$(MINUI_CORES_PATH)/config\" \
-	-DCORE_LIBS_PATH=\"$(MINUI_CORES_PATH)\" \
-	-DUSERDATA_PATH=\"$(MINUI_UI_PATH)/config\" \
-	-DSHARED_USERDATA_PATH=\"$(MINUI_SDCARD_PATH)/saves\" \
-	-DCORES_PATH=\"$(MINUI_CORES_PATH)\" \
-	-DROMS_PATH=\"$(MINUI_SDCARD_PATH)/roms\" \
-	-DBIOS_PATH=\"$(MINUI_SDCARD_PATH)/bios\" \
-	-DSAVES_PATH=\"$(MINUI_SDCARD_PATH)/saves\" \
-	-DPLATFORM=\"$(MINUI_PLATFORM_NAME)\" \
-	-DBUILD_DATE=\"$(MINUI_BUILD_DATE)\" \
-	-DBUILD_HASH=\"$(MINUI_BUILD_HASH)\"
-
-MINUI_SRC_DIR = $(@D)/src
-MINUI_ASSETS_DIR = $(@D)/assets
-MINUI_BUILD_DIR = $(@D)/build-minime
-MINUI_PLATFORM_DIR = $(MINUI_SRC_DIR)/platform/minime
-MINUI_PLATFORM_NAME = minime
-MINUI_BUILD_DATE = $(shell date +%Y.%m.%d)
-MINUI_BUILD_HASH = $(shell git -C $(@D) rev-parse --short HEAD 2>/dev/null || echo clean-start)
-MINUI_RUNTIME_RPATH = -Wl,-rpath,'$$ORIGIN'
-
-MINUI_DBUS_CFLAGS = -I$(STAGING_DIR)/usr/include/dbus-1.0 \
-	-I$(STAGING_DIR)/usr/lib/dbus-1.0/include
-MINUI_LIBRETRO_CFLAGS = -I$(STAGING_DIR)/usr/include
-MINUI_LZ4_CFLAGS = -I$(STAGING_DIR)/usr/include
-MINUI_LZ4_LDFLAGS = -L$(STAGING_DIR)/usr/lib
-
-define MINUI_BUILD_CMDS
-	mkdir -p $(MINUI_BUILD_DIR)
-
-	# libmsettings and platform helpers (shared by keymon, minarch and minui)
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC \
-		-I$(MINUI_SRC_DIR)/libmsettings \
-		-I$(MINUI_PLATFORM_DIR) \
-		-c $(MINUI_SRC_DIR)/libmsettings/msettings.c \
-		-o $(MINUI_BUILD_DIR)/msettings.o
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC -I$(MINUI_PLATFORM_DIR) -I$(MINUI_SRC_DIR)/common \
-		-c $(MINUI_PLATFORM_DIR)/traits.c \
-		-o $(MINUI_BUILD_DIR)/traits.o
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC -I$(MINUI_PLATFORM_DIR) -I$(MINUI_SRC_DIR)/common \
-		-c $(MINUI_PLATFORM_DIR)/audio.c \
-		-o $(MINUI_BUILD_DIR)/audio.o
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC -I$(MINUI_PLATFORM_DIR) -I$(MINUI_SRC_DIR)/common \
-		-c $(MINUI_PLATFORM_DIR)/video.c \
-		-o $(MINUI_BUILD_DIR)/platform-video.o
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC -I$(MINUI_PLATFORM_DIR) -I$(MINUI_SRC_DIR)/common \
-		-c $(MINUI_PLATFORM_DIR)/power.c \
-		-o $(MINUI_BUILD_DIR)/power.o
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC $(MINUI_CPPFLAGS) -DUSE_SDL2 \
-		-I$(MINUI_SRC_DIR)/common -I$(MINUI_PLATFORM_DIR) \
-		-I$(STAGING_DIR)/usr/include \
-		-c $(MINUI_SRC_DIR)/common/utils.c \
-		-o $(MINUI_BUILD_DIR)/utils.o
-	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC $(MINUI_CPPFLAGS) -DUSE_SDL2 \
-		-I$(MINUI_SRC_DIR)/common -I$(MINUI_PLATFORM_DIR) \
-		-I$(STAGING_DIR)/usr/include \
-		-c $(MINUI_SRC_DIR)/common/core_registry.c \
-		-o $(MINUI_BUILD_DIR)/core_registry.o
-	$(TARGET_CC) $(TARGET_LDFLAGS) -shared -Wl,-soname,libmsettings.so \
-		-o $(MINUI_BUILD_DIR)/libmsettings.so \
-		$(MINUI_BUILD_DIR)/msettings.o \
-		$(MINUI_BUILD_DIR)/traits.o \
-		$(MINUI_BUILD_DIR)/audio.o \
-		$(MINUI_BUILD_DIR)/platform-video.o \
-		$(MINUI_BUILD_DIR)/power.o \
-		$(MINUI_BUILD_DIR)/utils.o \
-		$(MINUI_BUILD_DIR)/core_registry.o -ldl -lrt
-
-	# keymon
-	$(TARGET_CC) $(TARGET_CFLAGS) \
-		-I$(MINUI_SRC_DIR)/common \
-		-I$(MINUI_SRC_DIR)/libmsettings \
-		-I$(MINUI_PLATFORM_DIR) \
-		$(MINUI_SRC_DIR)/keymon/keymon.c \
-		$(MINUI_PLATFORM_DIR)/input.c \
-		-o $(MINUI_BUILD_DIR)/keymon \
-		$(TARGET_LDFLAGS) $(MINUI_RUNTIME_RPATH) -L$(MINUI_BUILD_DIR) \
-		-lmsettings -lpthread -lrt -ldl
-
-	# minarch
-	$(TARGET_CC) $(TARGET_CFLAGS) $(MINUI_CPPFLAGS) -fomit-frame-pointer -std=gnu99 \
-		$(MINUI_LIBRETRO_CFLAGS) \
-		$(MINUI_LZ4_CFLAGS) \
-		-I$(MINUI_SRC_DIR)/minarch \
-		-I$(MINUI_SRC_DIR)/common \
-		-I$(MINUI_SRC_DIR)/libmsettings \
-		-I$(MINUI_PLATFORM_DIR) \
-		-DUSE_SDL2 \
-		$(MINUI_SRC_DIR)/minarch/main.c \
-		$(MINUI_SRC_DIR)/minarch/core.c \
-		$(MINUI_SRC_DIR)/minarch/content.c \
-		$(MINUI_SRC_DIR)/minarch/config.c \
-		$(MINUI_SRC_DIR)/minarch/options.c \
-		$(MINUI_SRC_DIR)/minarch/input.c \
-		$(MINUI_SRC_DIR)/minarch/rewind.c \
-		$(MINUI_SRC_DIR)/minarch/video.c \
-		$(MINUI_SRC_DIR)/minarch/menu.c \
-		$(MINUI_SRC_DIR)/common/scaler.c \
-		$(MINUI_SRC_DIR)/common/utils.c \
-		$(MINUI_SRC_DIR)/common/api.c \
-		$(MINUI_SRC_DIR)/common/core_registry.c \
-		$(MINUI_PLATFORM_DIR)/input.c \
-		$(MINUI_PLATFORM_DIR)/platform.c \
-		-o $(MINUI_BUILD_DIR)/minarch \
-		$(TARGET_LDFLAGS) $(MINUI_RUNTIME_RPATH) $(MINUI_LZ4_LDFLAGS) \
-		-L$(MINUI_BUILD_DIR) -ldl -llz4 -lmsettings -lSDL2 -lSDL2_image -lSDL2_ttf \
-		-lpthread -lm -lz
-
-	# minui main UI
-	$(TARGET_CC) $(TARGET_CFLAGS) $(MINUI_CPPFLAGS) -fomit-frame-pointer -std=gnu99 \
-		$(MINUI_DBUS_CFLAGS) \
-		-I$(MINUI_SRC_DIR)/main \
-		-I$(MINUI_SRC_DIR)/settings \
-		-I$(MINUI_SRC_DIR)/ui \
-		-I$(MINUI_SRC_DIR)/common \
-		-I$(MINUI_SRC_DIR)/libmsettings \
-		-I$(MINUI_PLATFORM_DIR) \
-		-DUSE_SDL2 \
-		$(MINUI_SRC_DIR)/main/main.c \
-		$(MINUI_SRC_DIR)/settings/settings.c \
-		$(MINUI_SRC_DIR)/settings/menu.c \
-		$(MINUI_SRC_DIR)/settings/jobs.c \
-		$(MINUI_SRC_DIR)/settings/timezone.c \
-		$(MINUI_PLATFORM_DIR)/wireless.c \
-		$(MINUI_PLATFORM_DIR)/wireless_bluetooth.c \
-		$(MINUI_SRC_DIR)/settings/about.c \
-		$(MINUI_SRC_DIR)/settings/power.c \
-		$(MINUI_SRC_DIR)/settings/time.c \
-		$(MINUI_SRC_DIR)/settings/wifi.c \
-		$(MINUI_SRC_DIR)/settings/bt.c \
-		$(MINUI_SRC_DIR)/settings/controls.c \
-		$(MINUI_SRC_DIR)/ui/badge.c \
-		$(MINUI_SRC_DIR)/ui/list.c \
-		$(MINUI_SRC_DIR)/ui/dialog.c \
-		$(MINUI_SRC_DIR)/ui/keyboard.c \
-		$(MINUI_SRC_DIR)/common/scaler.c \
-		$(MINUI_SRC_DIR)/common/utils.c \
-		$(MINUI_SRC_DIR)/common/api.c \
-		$(MINUI_SRC_DIR)/common/core_registry.c \
-		$(MINUI_PLATFORM_DIR)/input.c \
-		$(MINUI_PLATFORM_DIR)/platform.c \
-		-o $(MINUI_BUILD_DIR)/minui \
-		$(TARGET_LDFLAGS) $(MINUI_RUNTIME_RPATH) -L$(MINUI_BUILD_DIR) \
-		-ldbus-1 -ldl -lmsettings -lSDL2 -lSDL2_image -lSDL2_ttf -lpthread -lm -lz
-
-	# boot splash helper
-	$(TARGET_CC) $(TARGET_CFLAGS) \
-		-I$(MINUI_PLATFORM_DIR) \
-		$(MINUI_SRC_DIR)/show/show.c \
-		-o $(MINUI_BUILD_DIR)/minui-show \
-		$(TARGET_LDFLAGS) $(MINUI_RUNTIME_RPATH) -L$(MINUI_BUILD_DIR) \
-		-lmsettings -lSDL2 -lSDL2_image -lrt -ldl
-endef
 
 define MINUI_INSTALL_IMAGES_CMDS
 	mkdir -p $(BINARIES_DIR)/ui/.ui/bin
-	mkdir -p $(BINARIES_DIR)/ui/.ui/res
 	mkdir -p $(BINARIES_DIR)/ui/.ui/config
-	mkdir -p $(BINARIES_DIR)/ui/.cores/config
+	mkdir -p $(BINARIES_DIR)/ui/.ui/res
+	mkdir -p $(BINARIES_DIR)/ui/.cores
 
-	cp -f $(MINUI_BUILD_DIR)/minui $(BINARIES_DIR)/ui/.ui/bin/
-	cp -f $(MINUI_BUILD_DIR)/minarch $(BINARIES_DIR)/ui/.ui/bin/
-	cp -f $(MINUI_BUILD_DIR)/keymon $(BINARIES_DIR)/ui/.ui/bin/
-	cp -f $(MINUI_BUILD_DIR)/libmsettings.so $(BINARIES_DIR)/ui/.ui/bin/
-	cp -f $(MINUI_BUILD_DIR)/minui-show $(BINARIES_DIR)/ui/.ui/bin/
+	cd $(@D) && unzip -o MinUI.zip
 
-	$(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN' $(BINARIES_DIR)/ui/.ui/bin/minui
-	$(HOST_DIR)/bin/patchelf --set-rpath '$$ORIGIN' $(BINARIES_DIR)/ui/.ui/bin/minarch
+	cp -a $(@D)/.system/minime/. $(BINARIES_DIR)/ui/.ui/
+	cd $(BINARIES_DIR)/ui/.ui && for f in bin/*.elf; do \
+		[ -f "$$f" ] && mv -f "$$f" "bin/$$(basename "$$f" .elf)"; done
 
-	# shared MinUI assets
-	if [ -d $(MINUI_ASSETS_DIR)/res ]; then \
-		cp -a $(MINUI_ASSETS_DIR)/res/. $(BINARIES_DIR)/ui/.ui/res/; \
+	# Move libmsettings.so next to binaries
+	if [ -d $(@D)/.system/minime/lib ]; then \
+		cp -a $(@D)/.system/minime/lib/. $(BINARIES_DIR)/ui/.ui/bin/; \
+	fi
+
+	# Install cores
+	if [ -d $(@D)/.system/minime/cores ]; then \
+		cp -a $(@D)/.system/minime/cores/. $(BINARIES_DIR)/ui/.cores/; \
 	fi
 
 	# launcher entry point; UI-specific env setup
@@ -213,11 +53,6 @@ define MINUI_INSTALL_IMAGES_CMDS
 		'exec "$$SYSTEM_PATH/bin/minui"' \
 		> $(BINARIES_DIR)/ui/.ui/launch.sh
 	chmod +x $(BINARIES_DIR)/ui/.ui/launch.sh
-
-	# core default configs and controller binds
-	if [ -d $(MINUI_ASSETS_DIR)/cores ]; then \
-		cp -a $(MINUI_ASSETS_DIR)/cores/. $(BINARIES_DIR)/ui/.cores/config/; \
-	fi
 endef
 
 $(eval $(generic-package))
