f71e28e0874e31130a2a2d686c926af9a5289bf7
jheronimus <jheronimus@users.noreply.github.com>
2026-08-02 00:33:26 +0300

    fix(ci): pass root user to packager and set amd64 platform for buildroot

--- minime/targets/common.mk
+++ minime/targets/common.mk
@@ -2,19 +2,20 @@
 # Expects MINIME_ROOT, TARGET_NAME, BOARD, UI, PODMAN_CMD, PODMAN_TTY, PACKAGER_PLATFORM, PACKAGER_IMAGE to be defined.
 
 PACKAGER_IMAGE ?= $(BUILDER_IMAGE)
+PACKAGER_USER ?= --user 0:0
 
 .PHONY: image update clean
 
 image:
 	@mkdir -p $(CURDIR)/out/$(BOARD)
-	$(PODMAN_CMD) run $(PODMAN_TTY) --rm --platform $(PACKAGER_PLATFORM) \
+	$(PODMAN_CMD) run $(PODMAN_TTY) --rm $(PACKAGER_USER) --platform $(PACKAGER_PLATFORM) \
 		-v $(MINIME_ROOT):/workspace \
 		$(PACKAGER_IMAGE) \
 		sh -c "/workspace/minime/build/genassets.sh $(UI) /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD)/ui $(TARGET_NAME) && /workspace/minime/build/mkimage.sh --target $(TARGET_NAME) --board $(BOARD) --input-dir /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD) --output-dir /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD) && /workspace/minime/build/mkupdate.sh --target $(TARGET_NAME) --board $(BOARD) --ui $(UI) --input-dir /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD) --output-dir /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD)"
 
 update:
 	@mkdir -p $(CURDIR)/out/$(BOARD)
-	$(PODMAN_CMD) run $(PODMAN_TTY) --rm --platform $(PACKAGER_PLATFORM) \
+	$(PODMAN_CMD) run $(PODMAN_TTY) --rm $(PACKAGER_USER) --platform $(PACKAGER_PLATFORM) \
 		-v $(MINIME_ROOT):/workspace \
 		$(PACKAGER_IMAGE) \
 		sh -c "/workspace/minime/build/mkupdate.sh --target $(TARGET_NAME) --board $(BOARD) --ui $(UI) --input-dir /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD) --output-dir /workspace/minime/targets/$(TARGET_NAME)/out/$(BOARD)"
